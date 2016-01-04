2ec229cfd81a99b59b755a0b110796abc0aea584
umut özel <umutozel@outlook.com>
2016-01-04 16:42:52 +0200

    Removing WebActivatorEx

--- Beetle.Client/App_Start/WebApiConfig.cs
+++ Beetle.Client/App_Start/WebApiConfig.cs
@@ -5,8 +5,7 @@ namespace Beetle.Client.App_Start {
     public static class WebApiConfig {
 
         public static void Register(HttpConfiguration config) {
-            config.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}",
-                                       new { id = RouteParameter.Optional });
+            config.Routes.MapHttpRoute("BeetleApi", "api/{controller}/{action}/{id}", new { id = RouteParameter.Optional });
         }
     }
 }
