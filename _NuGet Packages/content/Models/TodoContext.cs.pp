ad1cdc96d0f1c5f870d77178571ac9522da3151c
umut özel <umutozel@outlook.com>
2016-01-05 09:33:05 +0200

    Updating nuget packages

--- _NuGet Packages/content/Models/TodoContext.cs.pp
+++ _NuGet Packages/content/Models/TodoContext.cs.pp
@@ -12,6 +12,7 @@ namespace $rootnamespace$.Models {
             var cs = Database.Connection.ConnectionString;
             cs = cs.Replace("Data Source=.", "Data Source=(localdb)");
             cs = cs.Replace("v12.0", "MSSQLLocalDB");
+            cs = cs.Replace("v13.0", "MSSQLLocalDB");
             cs = cs.Replace("SQLEXPRESS", "MSSQLLocalDB");
             Database.Connection.ConnectionString = cs;
 		}
