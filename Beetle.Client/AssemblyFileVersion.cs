ba6da9c5cbd52c1cc05e517b0eca12ca8dcdfc4b
umutozel <umutozel@gmail.com>
2016-05-01 17:24:00 +0300

    Helper structure changed.

--- Beetle.Client/AssemblyFileVersion.cs
+++ Beetle.Client/AssemblyFileVersion.cs
@@ -1,1 +1,1 @@
-﻿[assembly: System.Reflection.AssemblyFileVersion("2.0.2310.16")]
+﻿[assembly: System.Reflection.AssemblyFileVersion("2.0.2312.0")]
--- Beetle.Server.Mvc/AssemblyFileVersion.cs
+++ Beetle.Server.Mvc/AssemblyFileVersion.cs
@@ -1,1 +1,1 @@
-﻿[assembly: System.Reflection.AssemblyFileVersion("2.0.2310.2")]
+﻿[assembly: System.Reflection.AssemblyFileVersion("2.0.2312.0")]
