52fdc6114febff3fafbb33f72a4b267aaf8f3818
umut özel <umutozel@outlook.com>
2016-06-17 16:32:13 +0300

    Nuget update

--- Beetle.Client/Scripts/beetle/beetle.d.ts
+++ Beetle.Client/Scripts/beetle/beetle.d.ts
@@ -1,6 +1,6 @@
 ﻿// Type definitions for beetle.js 2.0
 // Project: https://github.com/umutozel/Beetle.js
-// File version: 2.0.11
+// File version: 2.0.12
 
 declare module beetle {
 
--- Samples/Beetle.Samples.Northwind/Scripts/beetle.js
+++ Samples/Beetle.Samples.Northwind/Scripts/beetle.js
@@ -10323,7 +10323,7 @@
     var beetle = (function () {
         return {
             // Export types
-            version: '2.0.11',
+            version: '2.0.12',
             registerI18N: function (code, i18n, active) {
                 i18Ns[code] = i18n;
                 if (active) i18N = i18n;
