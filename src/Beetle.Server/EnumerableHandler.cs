3cfe60861c5d142b3b0add6a50c19e74712fd81d
Umut Özel <umutozel@gmail.com>
2017-06-21 11:44:18 +0300

    Adding EnumerableHandler

---
+++ src/Beetle.Server/EnumerableHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Beetle.Server {
+    using Interface;
+    using Properties;
+
+    public class EnumerableHandler : IContentHandler<IEnumerable> {
+        private static readonly Lazy<EnumerableHandler> _instance = new Lazy<EnumerableHandler>();
+
+        public ProcessResult HandleContent(IEnumerable contentValue, IDictionary<string, string> parameters,
+                                           ActionContext actionContext, IBeetleService service = null) {
+            var maxResultCount = actionContext.MaxResultCount;
+            if (maxResultCount <= 0 && service != null && service.MaxResultCount > 0)
+                maxResultCount = service.MaxResultCount;
+
+            if (maxResultCount > 0) {
+                var count = Enumerable.Count((dynamic)contentValue);
+                if (count > maxResultCount)
+                    throw new BeetleException(Resources.ResultCountExceeded);
+            }
+
+            return new ProcessResult(actionContext) { Result = contentValue };
+        }
+
+        public static IContentHandler<IEnumerable> Instance => _instance.Value;
+    }
+}
