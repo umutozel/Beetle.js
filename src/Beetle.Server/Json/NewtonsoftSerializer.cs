6a8bc0a6667d8fe5abd4113be74439a6ffd7b506
Umut Özel <umutozel@gmail.com>
2017-06-21 10:18:13 +0300

    Adding newtonsoft serializer

---
+++ src/Beetle.Server/Json/NewtonsoftSerializer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+
+namespace Beetle.Server.Json {
+    using Interface;
+
+    public class NewtonsoftSerializer : ISerializer {
+
+        public NewtonsoftSerializer(JsonSerializerSettings settings) {
+            Settings = settings;
+            Settings.Converters.Add(new ExpandoObjectConverter());
+        }
+
+        public string Serialize(object obj) {
+            return JsonConvert.SerializeObject(obj, Settings);
+        }
+
+        public T Deserialize<T>(string str) {
+            return JsonConvert.DeserializeObject<T>(str, Settings);
+        }
+
+        public object Deserialize(string str, Type type) {
+            return JsonConvert.DeserializeObject(str, type, Settings);
+        }
+
+        public dynamic DeserializeToDynamic(string str) {
+            return JsonConvert.DeserializeObject<dynamic>(str, Settings);
+        }
+
+        public object ConvertFromDynamic(dynamic value, Type type) {
+            var reader = new JTokenReader(value);
+            return JsonSerializer.Create(Settings).Deserialize(reader, type);
+        }
+
+        public JsonSerializerSettings Settings { get; }
+    }
+}
