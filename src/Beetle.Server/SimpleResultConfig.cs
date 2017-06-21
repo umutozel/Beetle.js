62fdfe6e6f0be10d2c358760893cd84faf5e55de
Umut Özel <umutozel@gmail.com>
2017-06-21 10:22:02 +0300

    Adding SimpleResultConfig

---
+++ src/Beetle.Server/SimpleResultConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Beetle.Server {
+
+    public class SimpleResultConfig: BeetleConfig {
+        private static readonly Lazy<SimpleResultConfig> _instance = new Lazy<SimpleResultConfig>();
+
+        public SimpleResultConfig()
+            : base(NullValueHandling.Ignore, TypeNameHandling.None, PreserveReferencesHandling.None, Formatting.None) {
+        }
+
+        public new static SimpleResultConfig Instance => _instance.Value;
+    }
+}
