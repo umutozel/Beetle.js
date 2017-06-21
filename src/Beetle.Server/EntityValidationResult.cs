a5bdac2eb1d358b8b7609a65086cf576d73ff7b4
Umut Özel <umutozel@gmail.com>
2017-06-21 10:24:48 +0300

    Adding entity validation files

---
+++ src/Beetle.Server/EntityValidationResult.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Beetle.Server {
+
+    public class EntityValidationResult {
+
+        public EntityValidationResult(object entity, IEnumerable<ValidationResult> validationErrors) {
+            Entity = entity;
+            ValidationErrors = validationErrors;
+
+            IsValid = !ValidationErrors.Any();
+        }
+
+        public object Entity { get; }
+
+        public bool IsValid { get; }
+
+        public IEnumerable<ValidationResult> ValidationErrors { get; }
+    }
+}
