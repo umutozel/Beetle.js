00d600dc9731540e1adc4eb68f39610407cabd3f
Umut Özel <umutozel@gmail.com>
2017-06-22 16:33:36 +0300

    Adding tests

---
+++ tests/Beetle.Server.Tests/ServiceTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace Beetle.Server.Tests {
+    using Interface;
+
+    public class ServiceTests {
+        private readonly IBeetleService _service;
+
+        public ServiceTests() {
+        }
+
+        [Fact]
+        public void Test_Where() {
+        }
+    }
+}
