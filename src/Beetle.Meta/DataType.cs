96b8ff1de36bbf4362c0abd33cbc21b42d14dfd9
Umut Özel <umutozel@gmail.com>
2017-06-29 13:37:29 +0300

    Adding Meta project

---
+++ src/Beetle.Meta/DataType.cs
@@ -0,0 +1,18 @@
+﻿namespace Beetle.Meta {
+
+    public enum DataType {
+        String, 
+        Guid, 
+        Date, 
+        DateTimeOffset, 
+        Time, 
+        Boolean, 
+        Int, 
+        Number, 
+        Byte, 
+        Geography, 
+        Geometry, 
+        Binary,
+        Enum
+    }
+}
