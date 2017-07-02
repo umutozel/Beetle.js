d1c64098a8d66b6a10a35ee29b334cdd986a4b8b
Umut Ozel <umutozel@gmail.com>
2017-07-02 23:00:08 +0300

    Adding EF project

---
+++ src/Beetle.EntityFramework/EFQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Beetle.EntityFramework {
+    using Server;
+
+    public class EFQueryHandler: QueryableHandler {
+        private static readonly Lazy<EFQueryHandler> _instance = new Lazy<EFQueryHandler>(() => new EFQueryHandler());
+
+        public override IQueryable Skip(IQueryable query, int count) {
+            Expression<Func<int>> countAccessor = () => count;
+            var mi = typeof (QueryableExtensions).GetMethod("Skip").MakeGenericMethod(query.ElementType);
+            return mi.Invoke(null, new object[] { query, countAccessor }) as IQueryable;
+        }
+
+        public override IQueryable Take(IQueryable query, int count) {
+            Expression<Func<int>> countAccessor = () => count;
+            var mi = typeof(QueryableExtensions).GetMethod("Take").MakeGenericMethod(query.ElementType);
+            return mi.Invoke(null, new object[] { query, countAccessor }) as IQueryable;
+        }
+
+        public new static QueryableHandler Instance => _instance.Value;
+    }
+}
