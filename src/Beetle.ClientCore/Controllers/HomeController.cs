6fa0656e01206ca6970132f77d6adb757f3fd08f
Umut Özel <umutozel@gmail.com>
2017-07-07 10:13:54 +0300

    Adding Beetle.ClientCore project

---
+++ src/Beetle.ClientCore/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Beetle.ClientCore.Controllers {
+
+    public class HomeController : Controller {
+
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
